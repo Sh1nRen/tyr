14b282685e2e9d3701bd3303435131cea165b397
Sh1nRen <traxxxesss@gmail.com>
2019-01-12 11:58:16 +0300

    add some object

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,8 +1,19 @@
 public class MyFirstProgram{
 public static void main (String[] args){
 
-System.out.println("Greetings to you");
+hello("Vyacheslav");
+    Square square = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + square.l + " равна " + square.area());
 
-
+    Rectangle rectangle = new Rectangle(7,6);
+    System.out.println("Площадь прямоугольника со стороной " + rectangle.a + " и стороной " + rectangle.b + " равна " +
+            rectangle.area());
 }
+
+public static void hello(String name){
+    System.out.println("Greetings to you, " + name);
+}
+
+
+
 }
---
+++ sandbox/src/main/java/Square.java
@@ -0,0 +1,11 @@
+public class Square {
+    public double l;
+
+    public Square(double len) {
+        this.l = len;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
