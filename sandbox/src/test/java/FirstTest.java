610ac8ff28f32658c344903c60cce1b94c9b2e2a
Sh1nRen <traxxxesss@gmail.com>
2019-01-15 07:52:38 +0300

    add dependencies; add first test

---
+++ sandbox/src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+@Test
+    public void testArea() {
+    Square square = new Square(5);
+    Assert.assertEquals(square.area(), 25);
+}
+
+
+}
